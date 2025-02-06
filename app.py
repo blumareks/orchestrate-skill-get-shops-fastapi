5faf40aeeeb23dd4e5714ad49d01afe0e4f96678
MAREK SADOWSKI <mwsadows@us.ibm.com>
2025-02-06 07:24:35 -0800

    added phone

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ class RepairRequest(BaseModel):
 
 # Mocked list of repair shops
 repair_shops_db = [
-    {"name": "Speedy Auto Glass", "address": "123 Glass St, Pleasant Hill, CA"},
-    {"name": "Elite Windshield Repair", "address": "456 Auto Blvd, Oakland, CA"},
-    {"name": "Bay Area Glass Pros", "address": "789 Car Dr, San Francisco, CA"},
-    {"name": "Quick Fix Auto Glass", "address": "101 Fast Ln, San Mateo, CA"},
-    {"name": "Precision Windshield Experts", "address": "202 Car St, Berkeley, CA"},
+    {"name": "Speedy Auto Glass", "address": "123 Glass St, Pleasant Hill, CA","phone":"925-100-1234"},
+    {"name": "Elite Windshield Repair", "address": "456 Auto Blvd, Oakland, CA","phone":"925-100-1000"},
+    {"name": "Bay Area Glass Pros", "address": "789 Car Dr, San Francisco, CA","phone":"415-100-1000"},
+    {"name": "Quick Fix Auto Glass", "address": "101 Fast Ln, San Mateo, CA","phone":"510-100-1000"},
+    {"name": "Precision Windshield Experts", "address": "202 Car St, Berkeley, CA","phone":"925-100-1000"},
 ]
 
 @app.post("/get-repair-shops")
